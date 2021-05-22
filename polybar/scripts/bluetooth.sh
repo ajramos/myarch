7d99c0b002d6657626f4e8107444e453eb3cde3f
ajramos <tatto2k@gmail.com>
2021-05-23 01:02:03 +0200

    Improved bt script

--- polybar/scripts/bluetooth.sh
+++ polybar/scripts/bluetooth.sh
@@ -1,23 +1,31 @@
 #!/bin/sh
 
 COMMAND=$1
+BTACTIVE=$(systemctl status bluetooth | grep Active | sed -e 's/\s*Active:\s//g' \
+        | awk '{ split($0,a," "); print a[1]}')
 
-if [ "$COMMAND" = "status" ]; then
-  if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]; then
-    echo "%{F#66ffffff} Off"
-  else
-    if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 0 ]; then 
-      echo " On"
+
+if [ "$BTACTIVE" = "active" ]; then
+  if [ "$COMMAND" = "status" ]; then
+    if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]; then
+      echo "%{F#66ffff} Off"
     else
-      echo "%{F#2193ff} $(echo info | bluetoothctl | grep Name | sed -e 's/\s*Name:\s//g')"
+      if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 0 ]; then 
+        echo " On"
+      else
+        echo "%{F#2193ff} $(echo info | bluetoothctl | grep Name \
+                | sed -e 's/\s*Name:\s//g')"
+      fi
+    fi
+  else
+    if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]; then
+      bluetoothctl power on
+    else
+      bluetoothctl power off
     fi
   fi
 else
-  if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]; then
-    bluetoothctl power on
-  else
-    bluetoothctl power off
-  fi
+    echo "%{F#ff0000} Off"
 fi
 
 
