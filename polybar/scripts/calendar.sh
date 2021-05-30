1671c0c093b8b066ec76b5daf25f1cddfa9eccd1
ajramos <tatto2k@gmail.com>
2021-05-30 02:32:48 +0200

    Added week number to calendar

--- polybar/scripts/calendar.sh
+++ polybar/scripts/calendar.sh
@@ -2,7 +2,7 @@
 
 BAR_HEIGHT=22  # polybar height
 BORDER_SIZE=1  # border size from your wm settings
-YAD_WIDTH=222  # 222 is minimum possible value
+YAD_WIDTH=300  # 222 is minimum possible value
 YAD_HEIGHT=193 # 193 is minimum possible value
 DATE="$(date +" %H:%M")"
 
@@ -31,9 +31,9 @@ case "$1" in
         : $((pos_y = BAR_HEIGHT + BORDER_SIZE))
     fi
 
-    yad --calendar --undecorated --fixed --close-on-unfocus --no-buttons \
+    yad --calendar --show-weeks --undecorated --fixed --close-on-unfocus --no-buttons \
         --width="$YAD_WIDTH" --height="$YAD_HEIGHT" --posx="$pos_x" --posy="$pos_y" \
-        --title="yad-calendar" --borders=0 >/dev/null &
+        --title="calendar" --borders=0 >/dev/null &
     ;;
 *)
     echo "$DATE"
